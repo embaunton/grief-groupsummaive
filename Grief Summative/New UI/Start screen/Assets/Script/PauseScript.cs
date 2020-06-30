5883c5025acd070d90316b28ef928b3f30a528e6
Em <elbaunton@gmail.com>
2020-06-30 13:36:10 +1200

    ...

---
+++ Grief Summative/New UI/Start screen/Assets/Script/PauseScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+    [SerializeField] private bool isPaused;
+
+    private void Update()
+    {
+      if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+            //SoundManagerScript.PlaySound("Select");
+        }
+
+      if (isPaused)
+        {
+            ActivateMenu();
+        }
+
+      else
+        {
+            DeactivateMenu();
+        }
+
+    }
+
+    void ActivateMenu()
+    {
+        Time.timeScale = 0;
+        pauseMenuUI.SetActive(true);
+    }
+
+    public void DeactivateMenu()
+    {
+        Time.timeScale = 1;
+        pauseMenuUI.SetActive(false);
+        pauseMenuUI.SetActive(false);
+        isPaused = false;
+    }
+
+}
